5ad286dd271010b1b6fa46bdc53faa0729d186af
Rediet Yemane <redietamare04@gmail.com>
2024-05-31 18:22:58 +0300

    netflix clone final

---
+++ netflix-clone/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Banner from "./components/Banner/Banner";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import RowList from "./components/Rows/RowList/RowList";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Banner />
+      <RowList />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
